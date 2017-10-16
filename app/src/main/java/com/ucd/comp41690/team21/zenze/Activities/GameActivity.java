47a2e4c8ed423fba226da4c70ac285f3217a71a1
Annalena <bloch@in.tum.de>
2017-10-16 18:40:16 +0200

    inital commit

---
+++ app/src/main/java/com/ucd/comp41690/team21/zenze/Activities/GameActivity.java
@@ -0,0 +1,21 @@
+package com.ucd.comp41690.team21.zenze.Activities;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.ucd.comp41690.team21.zenze.R;
+
+/**
+ * the main game screen
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
---
+++ app/src/main/java/com/ucd/comp41690/team21/zenze/Activities/MainMenuActivity.java
@@ -0,0 +1,29 @@
+package com.ucd.comp41690.team21.zenze.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ucd.comp41690.team21.zenze.R;
+
+/**
+ * Main Activity that launches on start
+ * contains the Game Menu
+ */
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    /**
+     * is called when start-button is clicked
+     * @param v
+     */
+    public void startGame(View v){
+        startActivity(new Intent(MainMenuActivity.this, GameActivity.class));
+    }
+}
